444a6853762e1c48a594fe15b9a45065315f0e40
Alan Czemerinski <alanczeme@gmail.com>
2022-01-18 17:22:50 -0500

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const inputField = document.getElementById("new-task-description");
+
+  const formElement = document.querySelector("form");
+  formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(inputField.value);
+    inputField.value = '';
+  });
 });
+
+function handleToDo (task) {
+  let listItem = document.createElement("li");
+  listItem.innerText = `${task}`;
+  document.querySelector("#tasks").appendChild(listItem);
+}
